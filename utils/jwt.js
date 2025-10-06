2e3e1f8618e4b258f01b779c0a1d472b74a93ebb
Herbert Luis Ilagan <herb.ilagan@gmail.com>
2025-10-06 14:16:28 +0800

    Update module exports

--- utils/jwt.js
+++ utils/jwt.js
@@ -4,4 +4,4 @@ function generateToken(user) {
   return jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: "1h" });
 }
 
-module.exports = generateToken;
+module.exports = {generateToken};
