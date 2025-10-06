a3d22ebfd5d7ac2d231b9a5240edead93649b1df
Herbert Luis Ilagan <herb.ilagan@gmail.com>
2025-10-06 15:14:31 +0800

    Fix: use full callbackURL for Google strategy

--- config/passport-google.js
+++ config/passport-google.js
@@ -5,7 +5,7 @@ const User = require("../models/user");
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "/auth/google/callback"
+  callbackURL: "https://bible-auth-backend.onrender.com/auth/google/callback"
 }, async (accessToken, refreshToken, profile, done) => {
   console.log("🔍 Google profile received:", {
     id: profile.id,
