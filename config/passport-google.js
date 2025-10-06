75dd3868f4327c4947b996bae6e45632ec52417e
Herbert Luis Ilagan <herb.ilagan@gmail.com>
2025-10-06 14:28:54 +0800

    Fix google passport

--- config/passport-google.js
+++ config/passport-google.js
@@ -1,23 +1,27 @@
-const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const User = require("../models/user");
 
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: process.env.GOOGLE_CALLBACK_URL
+  callbackURL: "/auth/google/callback"
 }, async (accessToken, refreshToken, profile, done) => {
   try {
-    let user = await User.findOne({ googleId: profile.id });
-    if (!user) {
-      user = await User.create({
-        googleId: profile.id,
-        email: profile.emails?.[0]?.value || "",
-        name: profile.displayName
-      });
+    const existingUser = await User.findOne({ googleId: profile.id });
+
+    if (existingUser) {
+      return done(null, existingUser);
     }
-    return done(null, user);
+
+    const newUser = new User({
+      googleId: profile.id,
+      email: profile.emails[0].value
+    });
+
+    await newUser.save();
+    return done(null, newUser);
   } catch (err) {
+    console.error("❌ Error in Google strategy:", err);
     return done(err, null);
   }
 }));
